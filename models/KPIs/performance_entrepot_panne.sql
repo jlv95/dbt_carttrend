b06ec42369f77fdb628970431af0e1f088ce9139
KAHINA HAMAM <kahinahamam1978@gmail.com>
2025-05-28 09:50:09 +0000

    travail logistique et note client dans vue

---
+++ models/KPIs/performance_entrepot_panne.sql
@@ -0,0 +1,52 @@
+-- -calculer le total des volumes traités par chaque entrepôt
+-- - Le nombre de commandes avec un retard de livraison supérieur à 10 jours
+-- - les temps d’arrêt des machines par entrepôt
+-- - le nombre le nom et l'etat de machines en panne  
+WITH volumes_par_entrepot_type_etat AS (
+  SELECT
+    m.id_entrepot,
+    m.id_machine,
+    m.type_machine,
+    m.etat_machine,
+    SUM(m.volume_traite) AS total_volumes
+  FROM {{ ref('mrt_fct_machines') }} m  
+  GROUP BY m.id_entrepot, m.id_machine, m.type_machine, m.etat_machine
+),
+
+retards_par_entrepot AS (
+  SELECT
+    c.id_entrepot_depart,
+    COUNT(*) AS nb_commandes,
+    SUM(CASE WHEN DATE_DIFF(DATE(c.date_livraison_estimee), DATE(c.date_commande), DAY) > 10 THEN 1 ELSE 0 END) AS nb_retards
+  FROM {{ ref('mrt_fct_commandes') }} c  
+  GROUP BY c.id_entrepot_depart
+),
+
+pannes_par_entrepot_type_etat AS (
+  SELECT
+    m.id_entrepot,
+    m.id_machine,
+    m.type_machine,
+    m.etat_machine,
+    SUM(m.temps_arret) AS temps_panne_total
+  FROM {{ ref('mrt_fct_machines') }} m
+  GROUP BY m.id_entrepot, m.id_machine, m.type_machine, m.etat_machine
+)
+
+SELECT
+  e.id_entrepot,
+  e.localisation,
+  v.id_machine,
+  v.total_volumes,
+  r.nb_commandes,
+  r.nb_retards,
+  ROUND(100 * r.nb_retards / NULLIF(r.nb_commandes, 0), 2) AS taux_retards,
+  p.temps_panne_total,
+  v.type_machine,
+  v.etat_machine
+FROM {{ ref('mrt_dim_entrepots') }} e
+LEFT JOIN volumes_par_entrepot_type_etat v ON e.id_entrepot = v.id_entrepot
+LEFT JOIN retards_par_entrepot r ON e.id_entrepot = r.id_entrepot_depart
+LEFT JOIN pannes_par_entrepot_type_etat p 
+  ON e.id_entrepot = p.id_entrepot AND v.id_machine = p.id_machine
+ORDER BY total_volumes DESC
--- models/KPIs/performance_panne_machine.sql
+++ models/KPIs/performance_panne_machine.sql
@@ -1,51 +1,54 @@
--- -calculer le total des volumes traités par chaque entrepôt
--- - Le nombre de commandes avec un retard de livraison supérieur à 10 jours
--- (nb_retards)
--- - les temps d’arrêt des machines par entrepôt
--- - le nombre le nom et l'etat de machines en panne  
-WITH volumes_par_entrepot_type_etat AS (
+--- l'impact des pannes machines sur le volume traité et sur les retards de livraisons
+--- Le délai de livraison pour détecter s’il y a un retard (> 10 jours).
+--- le volume moyen traité par machine.
+--- le temps total d’arrêt de chaque machine
+WITH commandes_details AS (
   SELECT
+    c.id_commande,
+    c.date_commande,
+    c.date_livraison_estimee,
+    DATE_DIFF(DATE(c.date_livraison_estimee), DATE(c.date_commande), DAY) > 10 AS retard_livraison,
+    FORMAT_DATE('%Y-%m', DATE(c.date_livraison_estimee)) AS annee_mois,
+    c.id_entrepot_depart
+  FROM {{ ref('mrt_fct_commandes') }} c
+),
+
+machines_details AS (
+  SELECT
+    m.id_machine,
     m.id_entrepot,
     m.type_machine,
     m.etat_machine,
-    SUM(m.volume_traite) AS total_volumes
-  FROM {{ ref('mrt_fct_machines') }} m  
-  GROUP BY m.id_entrepot, m.type_machine, m.etat_machine
-),
-
-retards_par_entrepot AS (
-  SELECT
-    c.id_entrepot_depart,
-    COUNT(*) AS nb_commandes,
-    SUM(CASE WHEN DATE_DIFF(DATE(c.date_livraison_estimee), DATE(c.date_commande), DAY) > 10 THEN 1 ELSE 0 END) AS nb_retards
-  FROM {{ ref('mrt_fct_commandes') }} c  
-  GROUP BY c.id_entrepot_depart
-),
-
-pannes_par_entrepot_type_etat AS (
-  SELECT
-    m.id_entrepot,
-    m.type_machine,
-    m.etat_machine,
-    SUM(m.temps_arret) AS temps_panne_total, -- voir le temps darret (mrt-fct-machine)
-    COUNT(CASE WHEN m.etat_machine = 'en panne' THEN 1 END) AS nb_pannes
+    m.volume_traite,
+    m.temps_arret
   FROM {{ ref('mrt_fct_machines') }} m
-  GROUP BY m.id_entrepot, m.type_machine, m.etat_machine
+),
+
+joined_data AS (
+  SELECT
+    cd.id_entrepot_depart AS id_entrepot,
+    cd.retard_livraison,
+    cd.annee_mois,
+    md.id_machine,
+    md.type_machine,
+    md.etat_machine,
+    md.volume_traite,
+    md.temps_arret
+  FROM commandes_details cd
+  LEFT JOIN machines_details md ON cd.id_entrepot_depart = md.id_entrepot
 )
 
 SELECT
-  e.id_entrepot,
-  e.localisation,
-  v.total_volumes,
-  r.nb_commandes,
-  r.nb_retards,
-  ROUND(100 * r.nb_retards / NULLIF(r.nb_commandes, 0), 2) AS taux_retards,
-  p.nb_pannes,
-  p.temps_panne_total,
-  v.type_machine,
-  v.etat_machine
-FROM {{ ref('mrt_dim_entrepots') }} e
-LEFT JOIN volumes_par_entrepot_type_etat v ON e.id_entrepot = v.id_entrepot
-LEFT JOIN retards_par_entrepot r ON e.id_entrepot = r.id_entrepot_depart
-LEFT JOIN pannes_par_entrepot_type_etat p ON e.id_entrepot = p.id_entrepot
-ORDER BY total_volumes DESC
+  id_entrepot,
+  id_machine,
+  type_machine,
+  etat_machine,
+  COUNT(*) AS nb_commandes,
+  SUM(CASE WHEN retard_livraison THEN 1 ELSE 0 END) AS nb_retards,
+  ROUND(100 * SUM(CASE WHEN retard_livraison THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0), 2) AS taux_retards,
+  ROUND(AVG(volume_traite), 2) AS volume_moyen_traite,
+  SUM(temps_arret) AS total_temps_arret
+FROM joined_data
+GROUP BY id_entrepot, id_machine, type_machine, etat_machine
+ORDER BY taux_retards DESC
+
