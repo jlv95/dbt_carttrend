99f4f7f2c04693e28431b64fd535722e16124f0f
Jean-Laurent Viarengo-Roccaserra <viarengo.jeanlaurent+1@gmail.com>
2025-06-03 16:48:15 +0000

    Ajout de commentaires sur quasi tous les fichiers stg et mrt

--- models/marts/mrt_dim_clients.sql
+++ models/marts/mrt_dim_clients.sql
@@ -4,7 +4,5 @@
     materialized='table'
 ) }}
 
--- je ne sélectionne que les champs voulus (ex : je ne sélectionne pas les champs à caractère personnel, car ils n'ont pas leur utilité en BI)
-
 SELECT *
 FROM {{ ref('stg_clients') }}
